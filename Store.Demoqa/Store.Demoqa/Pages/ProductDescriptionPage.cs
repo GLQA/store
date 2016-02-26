03d2e90da769fab730280067d87b3d23c90513a1
maryna.kovalenko <maryna.kovalenko@globallogic.com>
2016-02-26 18:05:00 +0200

    First test

---
+++ Store.Demoqa/Store.Demoqa/Pages/ProductDescriptionPage.cs
@@ -0,0 +1,40 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+namespace Store.Demoqa
+{
+    public class ProductDescriptionPage
+    {
+        private IWebDriver driver;
+
+        public ProductDescriptionPage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+
+        [FindsBy(How = How.CssSelector, Using = ".prodtitle")]
+        private IWebElement ProductTitle;
+
+        [FindsBy(How = How.CssSelector, Using = ".product_description")]
+        private IWebElement ProductDescription;
+
+        [FindsBy(How = How.CssSelector, Using = ".wpsc_also_bought.group")]
+        private IWebElement PeopleWhoBoughtThisItemSection;
+
+        public string GetTitleText()
+        {
+            return ProductTitle.Text;
+        }
+
+        public string GetDescriptionText()
+        {
+            return ProductDescription.Text;
+        }
+
+        public string GetTextOfPeopleWhoBoughtSection()
+        {
+            return PeopleWhoBoughtThisItemSection.Text;
+        }
+    }
+}
--- Store.Demoqa/Store.Demoqa/Test.cs
+++ Store.Demoqa/Store.Demoqa/Test.cs
@@ -1,7 +1,8 @@
-﻿using System;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
 
 namespace Store.Demoqa
 {
@@ -9,12 +10,13 @@ namespace Store.Demoqa
     /// Store.Demoqa Tests
     /// </summary>
     [TestFixture]
-    public class Test
+    public class TestSuite
     {
+        private const string SITEURL = "http://store.demoqa.com/";
         /// <summary>
         /// driver declaration
         /// </summary>
-       private IWebDriver driver;
+        public static IWebDriver driver;
 
         /// <summary>
         /// Start Firefox browser
@@ -22,25 +24,48 @@ namespace Store.Demoqa
         [SetUp]
         public void Init()
         {
-            this.driver = new FirefoxDriver(); 
+            driver = new FirefoxDriver();
+            driver.Navigate().GoToUrl(SITEURL);
         }
 
-        /// <summary>
-        /// Open site http://store.demoqa.com/
-        /// </summary>
+
         [Test]
-        public void OpenSiteTest()
+        public void ProductContentVerification()
         {
-           this.driver.Navigate().GoToUrl("http://store.demoqa.com/");
+            Footer footer = new Footer(driver);
+            string firstProductTitle = footer.GetTitleText().TrimEnd('-', '.');
+            ProductDescriptionPage prodPage = footer.GoToRandomProduct();
+            StringAssert.StartsWith(firstProductTitle, prodPage.GetTitleText());
+            Assert.IsNotEmpty(prodPage.GetDescriptionText());
+            Assert.IsNotEmpty(prodPage.GetTextOfPeopleWhoBoughtSection());
         }
 
+        [Test]
+        public void SearchFunctionalityVerification()
+        {
+
+        }
+
+        [Test]
+        public void PictureEnlargementVerification()
+        {
+
+        }
+
+        [Test]
+        public void VerificationOfLikeFunctionality()
+        {
+
+        }
         /// <summary>
         /// Close current opened tab in browser
         /// </summary>
         [TearDown]
         public void Close()
         {
-            this.driver.Close();
+            driver.Quit();
         }
+
+        
     }
 }

cbee9870a60e405d7aafa8455c7a392e18629b45
maryna.kovalenko <maryna.kovalenko@globallogic.com>
2016-03-24 11:36:49 +0200

    changes assembly name

--- Store.Demoqa/Store.Demoqa/Properties/AssemblyInfo.cs
+++ Store.Demoqa/Store.Demoqa/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Store.Demoqa")]
+[assembly: AssemblyTitle("Store")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("______")]
-[assembly: AssemblyProduct("Store.Demoqa")]
+[assembly: AssemblyProduct("Store")]
 [assembly: AssemblyCopyright("Copyright © ______ 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
